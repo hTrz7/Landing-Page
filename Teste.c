c0e306f845cef09d7b48a5387f794f94e46058cd
Heitor <heitorfr007@gmail.com>
2024-07-24 13:11:28 -0300

    primeiro commit teste

---
+++ Teste.c
@@ -0,0 +1,21 @@
+/*
+    Escreva um programa que receba o salário de um funcionário e
+    calcule quanto ele precisa pagar de imposto de renda (aliquota 25%)
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    float salario, calc, ir;
+
+    printf("informe o seu salario: ");
+    scanf("%f", &salario);
+
+    calc = (salario * 0.25) / 100;
+    ir = calc - 873.6;
+
+    printf("voce tera que pagar %.2f de imposto de renda.", ir);
+
+    return 0;
+}
