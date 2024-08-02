f7f37ac600df2c66fca459e4119d2290aae2899c
Heitor <heitorfr007@gmail.com>
2024-08-02 09:48:18 -0300

    updating fatorial

--- Teste/resumo.c
+++ Teste/resumo.c
@@ -7,9 +7,9 @@ int main (){
     printf("informe um numero: ");
     scanf("%d", &num);
 
-    for (int i = num -1; i >= 0; i--){
+    for (int i = num - 1; i > 0; i--){
         num = num * i;
-        printf("%d\n", num);
+        printf("x %d = %d\n",i, num);
     }
 
     return 0;
