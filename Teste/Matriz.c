99c3970658bd6a074ba235091337c45bad5aa80a
Heitor <heitorfr007@gmail.com>
2024-09-30 19:01:16 -0300

    soma das diagonais da matriz

--- Teste/Matriz.c
+++ Teste/Matriz.c
@@ -1,25 +1,32 @@
-//armazenar valor de rendas per capitas//
+/*
+    Escreva um programa que receba 9 numeros e os armazene em uma matriz 3x3.
+    A sa√≠da do programa dever√° ser a soma dos numeros da diagonal.
+*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
 
-    float matriz[4][3];
+    int matriz[3][3], soma = 0, soma2 = 0;
 
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            printf("Informe a renda: ");
-            scanf("%f", &matriz[i][j]);
+            printf("insira os n˙meros da matriz: linha(%d) coluna(%d)", i, j);
+            scanf("%d", &matriz[i][j]);
         }
     }
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 3; j++){
-            printf("%.2f \n", matriz[i][j]);
-        }
+    for(int i = 0; i < 3; i++){    
+        soma += matriz[i][i];    
     }
+    for(int i = 2; i >= 0; i--){
+        soma2 += matriz[i][i];
+    }
+    printf("A soma dos numeros da diagonal esquerda superior ate direita inferior È: %d \n", soma);
+    printf("A soma dos numeros da diagonal direita superior ate a esquerda inferior È: %d", soma2);
 
     return 0;
 }
